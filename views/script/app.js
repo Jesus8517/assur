695a5a7360211c3df54ce967fcd47c63d15a7525
Jesus8517 <valentin.sainz1@gmail.com>
2017-01-26 11:37:01 +0100

    list

--- views/script/app.js
+++ views/script/app.js
@@ -2,7 +2,47 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('assurCtrl', ['$scope', function($scope) {
 
-    $scope.test = 'allalalala';
+  $scope.clients = [
+    {
+      name:'Roger',
+      cars: {
+        name:''
+      }
+    }, {
+      name:'Melusine',
+      cars: {
+        name: 'Opel',
+        assuree: 'oui'
+      }
+    }, {
+      name:'Billy',
+      cars: [{
+        name:'Aston Martin',
+        assuree: 'oui'
+      }, {
+        name: 'Audi',
+        assuree: 'non'
+      }]
+    }, {
+      name:'Gertrude',
+      cars: [{
+        name: 'Merco',
+        assuree: 'oui'
+      }, {
+        name: 'Honda',
+        assuree: 'oui'
+      }, {
+        name: 'Peugeot',
+        assuree: 'non'
+      }]
+    }, {
+      name:'Dominique',
+      cars: {
+        name:'Tesla',
+        assuree: 'non'
+      }
+    }
+  ]
 
 
 }])
